8e53e7f06ee16ba15b0c6dedc87e9950b8788ef9
Ricardo Tenorio <ricardobatenorio@gmail.com>
2021-03-09 12:17:58 -0300

    frist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  
+  passwordLength = 0;
+  texto: string = '';
+  useLetters: boolean = false;
+  useNumbers: boolean = false;
+  useSymbols: boolean = false;
+
+  onChangeLength(event: Event ) {
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+    
+    if (!isNaN(parsedValue)) {
+      this.passwordLength = parsedValue;
+    }
+  }
+
+  onChangeUseLetters () {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseNumbers () {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeUseSymbols () {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onButtonClick() {
+    if(this.texto === '')
+    {
+      this.texto = 'mudou';
+    } else {
+      this.texto = '';
+    }
+  }
+
 }
